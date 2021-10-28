8585a1d420cb8f8e75513b11465dbcb91a804295
George Duarte <contato@georgeduarte.com.br>
2021-10-28 05:40:45 -0300

    Data access by repository - updates

--- src/Dev.IO.App/Startup.cs
+++ src/Dev.IO.App/Startup.cs
@@ -4,17 +4,11 @@ using Dev.IO.Data.Context;
 using Dev.IO.Data.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Dev.IO.App
 {
