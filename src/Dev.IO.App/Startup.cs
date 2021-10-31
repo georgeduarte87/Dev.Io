5a6da6f34b8c952e4839c18152b759acb38600de
George Duarte <contato@georgeduarte.com.br>
2021-10-31 14:54:01 -0300

    Separete Globalizations from Startup

--- src/Dev.IO.App/Startup.cs
+++ src/Dev.IO.App/Startup.cs
@@ -66,14 +66,7 @@ namespace Dev.IO.App
             app.UseAuthentication();
             app.UseAuthorization();
 
-            var defaultCulture = new CultureInfo("pt-BR");
-            var localizationOptions = new RequestLocalizationOptions
-            {
-                DefaultRequestCulture = new RequestCulture(defaultCulture),
-                SupportedCultures = new List<CultureInfo> { defaultCulture },
-                SupportedUICultures = new List<CultureInfo> { defaultCulture }
-            };
-            app.UseRequestLocalization(localizationOptions);
+            app.UseGlobalizatonConfig();
 
             app.UseEndpoints(endpoints =>
             {
