832dee38e67d59e7ccf71a4d68ffa8f84c319b22
George Duarte <contato@georgeduarte.com.br>
2021-11-01 09:00:08 -0300

    Configure Notifications Services - Part 1

--- src/Dev.IO.Business/Services/BaseService.cs
+++ src/Dev.IO.Business/Services/BaseService.cs
@@ -1,4 +1,6 @@
-﻿using Dev.IO.Business.Models;
+﻿using Dev.IO.Business.Interfaces;
+using Dev.IO.Business.Models;
+using Dev.IO.Business.Notifications;
 using FluentValidation;
 using FluentValidation.Results;
 
@@ -6,6 +8,13 @@ namespace Dev.IO.Business.Services
 {
     public abstract class BaseService
     {
+        private readonly INotificador _notificador;
+
+        protected BaseService(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
         protected void Notificar(ValidationResult validationResult)
         {
             foreach (var error in validationResult.Errors)
@@ -17,7 +26,7 @@ namespace Dev.IO.Business.Services
 
         protected void Notificar(string mensagem)
         {
-
+            _notificador.Handle(new Notificacao(mensagem));
         }
 
         protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) 
@@ -32,6 +41,5 @@ namespace Dev.IO.Business.Services
 
             return false;
         }
-
     }
 }
